069f8e1e2a7ebbeaf8e017389aaaae5d45b666f8
Allanflo88 <allanfloresdejesus@hotmail.com>
2017-10-08 13:46:06 -0300

    Implements Total Itens

--- main.py
+++ main.py
@@ -10,9 +10,17 @@ while(1):
         break
     price = input("Preco:")
     produtos.append(model.produto(c,descr, price))
+    print("Digite a quantidade do item: " + str(produtos[c - 1].getDescr()))
+    produtos[c - 1].setQtd(input())
 
 
-print("Descricao" + "   " + "Valor Unitario")
+print("Descricao" + "   " + "Valor Unitario" + "    " + "Quantidade")
 for c in range(len(produtos)):
-    print(str(produtos[c].getDescr()) + '   ' + str(produtos[c].getValUni()))
+    print(str(produtos[c].getDescr()) + "           " + str(produtos[c].getValUni()) + "            " + str(produtos[c].getQtd()))
     c += 1
+
+totalItens = 0.0
+for c in range(len(produtos)):
+    totalItens += float(produtos[c].getValUni()) * float(produtos[c].getQtd())
+
+print("Valor total dos itens: " + str(totalItens))

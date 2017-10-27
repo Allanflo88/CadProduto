436ad83e5b8a703336d064385ff8ddd410aaf1ab
Caique Siqueira <cslclaman@gmail.com>
2017-10-27 12:23:06 -0200

    Include SQL drop and create commands

--- mainWithDataBase.py
+++ mainWithDataBase.py
@@ -9,8 +9,11 @@ produtos = []
 con = MySQLdb.connect(host="localhost",user="root",passwd="Andreia16",db="Testes")
 cursor = con.cursor()
 
-x = int(input("deseja criar Banco de dados? 1 - sim ou 2 - não "))
+x = int(input("deseja (re)criar Banco de dados? 1 - sim ou 2 - não "))
 if x == 1:
+    cursor.execute("drop database if exists Testes")
+    cursor.execute("create database Testes")
+
     cursor.execute("drop table if exists produto")
 
     sql = """create table produto(cod int auto_increment primary key,descr varchar(20) not null,valUni float(5) not null,qtd int);"""
